78e914ec75e774cf5e00579edba70f1e186b8559
sahabajahamed <sahabaj.ahammed2022@gmail.com>
2024-11-05 19:45:26 +0530

    Reimbursementapprove button working

--- src/test/java/testBase/Test1.java
+++ src/test/java/testBase/Test1.java
@@ -3,10 +3,12 @@ package testBase;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.io.File;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -185,8 +187,154 @@ public class Test1 {
 
 	}
 	@Test
-	public void addReim()
+	public void VerifyAddReimbursement() throws InterruptedException, AWTException
 	{
+
+		Thread.sleep(5000);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		// Thread.sleep(5000);
+		// js.executeScript("document.body.style.zoom = '0.7';");
+		Robot robot = new Robot();
+		Thread.sleep(5000);
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_SUBTRACT);
+		robot.keyRelease(KeyEvent.VK_SUBTRACT);
+		robot.keyPress(KeyEvent.VK_SUBTRACT);
+		robot.keyRelease(KeyEvent.VK_SUBTRACT);
+		robot.keyRelease(KeyEvent.VK_SUBTRACT);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		driver.findElement(By.xpath("//input[@id='exampleInputUsername']")).sendKeys(" Cunex Inc.");
+		driver.findElement(By.xpath("//input[@placeholder='Enter password']")).sendKeys("Thenewpasswordiscunex@362");
+		driver.findElement(By.xpath("//button[@class='btn full-btn']")).click();
+		driver.findElement(By.xpath("//a[@id='step-human-resources']")).click();
+		driver.findElement(By.xpath("//a[@id='arrow-employees']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//a[@id='reimbursements']")).click();
+		String mainWindow = driver.getWindowHandle();
+
+		Set<String> allWindows = driver.getWindowHandles();
+		for (String window : allWindows) {
+			if (!window.equals(mainWindow)) {
+				driver.switchTo().window(window); // Switch to new window
+				break;
+			}
+
+		}
+		Thread.sleep(5000);
+		int countreim = 3;
+		while (countreim != 0) {
+			driver.findElement(By.xpath("//button[@data-bs-toggle='modal']")).click();
+			Thread.sleep(5000);
+			driver.findElement(By.id("select2-user_id-container")).click();
+			Thread.sleep(5000);
+			List<WebElement> SuggestionName = driver.findElements(By.xpath("//ul[@id='select2-user_id-results']//li"));
+
+			Random random2 = new Random();
+			int randomName = random2.nextInt(SuggestionName.size());
+			WebElement randomSuggestion = SuggestionName.get(randomName);
+			randomSuggestion.click();
+			Thread.sleep(5000);
+			driver.findElement(By.xpath("//input[@id='date']")).click();
+			List<WebElement> allDates = driver
+					.findElements(By.xpath("//table[@class='ui-datepicker-calendar']//td[@data-event='click']"));
+			if (!allDates.isEmpty()) {
+				Random random = new Random();
+				int randomIndex = random.nextInt(allDates.size());
+				WebElement randomDate = allDates.get(randomIndex);
+				randomDate.click();
+
+			} else {
+				System.out.println("No selectable dates found.");
+			}
+			driver.findElement(By.xpath("//input[@id='amount']")).sendKeys("230");
+			driver.findElement(By.xpath("//textarea[@id='description']")).sendKeys("Automate selenium script ");
+			WebElement chouseImage = driver.findElement(By.xpath("//input[@id='images']"));
+			File file = new File("src/images/screenshot-staging_groundmetrx_com-2024_11_04-16_59_47.png");
+			String absoulutePath = file.getAbsolutePath();
+			chouseImage.sendKeys(absoulutePath);
+			driver.findElement(By.xpath("//button[normalize-space()='Create']")).click();
+			Thread.sleep(8000);
+			countreim--;
+		}
+
+	}
+	
+
+	@Test
+	public void verifyAdminApproveOrRejectReim() throws InterruptedException, AWTException
+	{
+		Thread.sleep(5000);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		// Thread.sleep(5000);
+		// js.executeScript("document.body.style.zoom = '0.7';");
+		Robot robot = new Robot();
+		Thread.sleep(5000);
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_SUBTRACT);
+		robot.keyRelease(KeyEvent.VK_SUBTRACT);
+		robot.keyPress(KeyEvent.VK_SUBTRACT);
+		robot.keyRelease(KeyEvent.VK_SUBTRACT);
+		robot.keyRelease(KeyEvent.VK_SUBTRACT);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		driver.findElement(By.xpath("//input[@id='exampleInputUsername']")).sendKeys(" Cunex Inc.");
+		driver.findElement(By.xpath("//input[@placeholder='Enter password']")).sendKeys("Thenewpasswordiscunex@362");
+		driver.findElement(By.xpath("//button[@class='btn full-btn']")).click();
+		driver.findElement(By.xpath("//a[@id='step-human-resources']")).click();
+		driver.findElement(By.xpath("//a[@id='arrow-employees']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//a[@id='reimbursements']")).click();
+		String mainWindow = driver.getWindowHandle();
+
+		Set<String> allWindows = driver.getWindowHandles();
+		for (String window : allWindows) {
+			if (!window.equals(mainWindow)) {
+				driver.switchTo().window(window); // Switch to new window
+				break;
+			}
+
+		}
+		Thread.sleep(5000);
+
+		List<WebElement> viewALLbutton = driver.findElements(By.xpath("//tbody//tr//td//button[text()='View']"));
 		
+		
+		
+		for (WebElement viewButton : viewALLbutton) 
+		{
+			try
+			{
+				viewButton.click();
+			Thread.sleep(4000);
+			WebElement approveButton = driver.findElement(By.xpath("//button[normalize-space()='Approve']"));
+			WebElement rejecteButton = driver.findElement(By.xpath("//button[normalize-space()='Reject']"));
+
+
+			if(new Random().nextBoolean())
+			{
+				approveButton.click();
+				Thread.sleep(4000);
+				driver.findElement(By.xpath("//button[normalize-space()='Yes']")).click();
+				Thread.sleep(5000);
+				System.out.println("Approve Button click Sucessfully");
+			}
+			else
+			{
+				rejecteButton.click();
+				driver.findElement(By.xpath("//button[normalize-space()='Yes']")).click();
+				Thread.sleep(5000);
+				System.out.println("Reimbursement is Rejected ");
+			}
+
+			}catch(StaleElementReferenceException e)
+			{
+				System.out.println("Encountered StaleElementReferenceException, retrying...");
+				viewALLbutton=driver.findElements(By.xpath("//tbody//tr//td//button[text()='View']"));
+
+			}
+			
+			
+			
+		}
+
 	}
 }
