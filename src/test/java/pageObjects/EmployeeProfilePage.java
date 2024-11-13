2f109de803ffa3e68a1df7b434a524864c2f87df
sahabajahamed <sahabaj.ahammed2022@gmail.com>
2024-11-13 19:31:39 +0530

    changes

--- src/test/java/pageObjects/EmployeeProfilePage.java
+++ src/test/java/pageObjects/EmployeeProfilePage.java
@@ -3,6 +3,8 @@ package pageObjects;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Random;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,7 +20,7 @@ public class EmployeeProfilePage extends BasePage {
 
     }
     
-    //Basic Inforamtion Xpath
+    //-------------------------------------------------------------Basic Inforamtion Xpath------------------------------------------//
     @FindBy(xpath = "//button[@id='basic_info_edit_btn']")
     private WebElement editButton;
     @FindBy(xpath = "//input[@id='emp_start_date']")
@@ -27,24 +29,26 @@ public class EmployeeProfilePage extends BasePage {
     private List<WebElement> startDate;
     @FindBy(xpath = "//input[@id='username']")
     private WebElement username;
-    @FindBy(xpath = "//select[@id='role_id']")                      //SelectClass
+    @FindBy(xpath = "//select[@id='role_id']")                      //--SelectClass--//
     private WebElement role;
     @FindBy(xpath = "//input[@id='phone_number']")
     private WebElement phone;
     @FindBy(xpath = "//input[@id='fedex_id']")
     private WebElement fedExId;
     @FindBy(xpath = "//input[@id='home_terminal']")
-    private WebElement homeTerminal; // input 1 in hometermianl 
-    @FindBy(xpath = "//select[@id='department']")                       //SelectClass
+    private WebElement homeTerminal;                                      // input 1 in hometermianl 
+    @FindBy(xpath = "//select[@id='department']")                       //--SelectClass--//
     private WebElement depearment;
     @FindBy(xpath = "//input[@id='position']")
     private WebElement position;
-    @FindBy(xpath = "//select[@id='entity_id']")                            //SelectClass
+    @FindBy(xpath = "//select[@id='entity_id']")                            //--SelectClass--//
     private WebElement entity;
     @FindBy(xpath = "//select[@id='type']")//SelectClass
     private WebElement type;
     @FindBy(xpath = "//input[@id='manager_name']")
     private WebElement manager;
+    @FindBy(xpath = " //ul[@id='ui-id-3']//li")
+    private List<WebElement> suggesationManagername;
     @FindBy(xpath = "//input[@id='card_expiration_date']")
     private WebElement dotCardExcreption;
     @FindBy(xpath = "//input[@id='license_expiration_date']")
@@ -60,7 +64,8 @@ public class EmployeeProfilePage extends BasePage {
     private WebElement personalTab;
     
 
-    //Emergencey contact
+    //------------------------------------------------Emergencey contact--------------------------------------------------------//
+
     @FindBy(xpath = "//button[@id='emergency_contacts_info_add_btn']")
     private WebElement addNewContact;
     @FindBy(xpath = " (//input[@id='emergency_contacts[0][first_name]'])[1]")
@@ -78,7 +83,7 @@ public class EmployeeProfilePage extends BasePage {
     @FindBy(xpath = "//button[@id='emergency_contacts_info_cancel_btn']")
     private WebElement emergencyCancelButton;
 
-    //Payroll
+    //----------------------------------------------------Payroll---------------------------------------------------------------------//
     @FindBy(xpath = "//input[@id='emp_id']")
     private WebElement payrollEmpID;
     @FindBy(xpath = "//select[@id='entity_id']")
@@ -97,6 +102,108 @@ public class EmployeeProfilePage extends BasePage {
     private WebElement fuelPay;
     
 
+    //// --------------------------------------------------Methods for Basic Information----------------------------------------/////
+    public void clickEditButton() {
+        editButton.click();
+    }
+
+    public void setStartDate(String date) {
+        startDateClick.click();
+        if (!startDate.isEmpty()) {
+            Random random = new Random();
+            int randomIndex = random.nextInt(startDate.size());
+            WebElement randomDate = startDate.get(randomIndex);
+            randomDate.click();
+        }
+    }
+    public void enterUsername(String name) {
+        username.clear();
+        username.sendKeys(name);
+    }
+
+    public void selectRole(String roleName) {
+        getSelect(role).selectByVisibleText(roleName);
+    }
+
+    public void enterPhone(String phoneNumber) {
+        phone.clear();
+        phone.sendKeys(phoneNumber);
+    }
+    public void enterFedExId(String fedExIdValue) {
+        fedExId.clear();
+        fedExId.sendKeys(fedExIdValue);
+    }
+
+    public void enterHomeTerminal(String terminal) {
+        homeTerminal.clear();
+        homeTerminal.sendKeys(terminal);
+    }
+
+    public void selectDepartment(String dept) {
+        getSelect(depearment).selectByVisibleText(dept);
+    }
+
+    public void enterPosition(String positionName) {
+        position.clear();
+        position.sendKeys(positionName);
+    }
+
+    public void selectEntity(String entityName) {
+         getSelect(entity).selectByVisibleText(entityName);
+    }
+
+    public void selectType(String typeName) {
+        getSelect(type).selectByVisibleText(typeName);
+    }
+    public void enterManager(String managerName) {
+        manager.clear();
+        manager.sendKeys(managerName);
+    }
+
+    public void setDotCardExpiration(String expirationDate) {
+        dotCardExcreption.sendKeys(expirationDate);
+    }
+
+    public void setLicenseExpiration(String expirationDate) {
+        licenseExpiration.sendKeys(expirationDate);
+    }
+
+    public void selectShiftTime(String shift) {
+        getSelect(shiftTime).selectByVisibleText(shift);
+    }
+
+    public void selectEmpType(String empTypeName) {
+        getSelect(empType).selectByVisibleText(empTypeName);
+    }
+
+
+    
+   
     
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
